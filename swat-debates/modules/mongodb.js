b58fd706c52cdad8a822961b27a499b128b252ac
Dylan <djacobs2@swarthmore.edu>
2025-06-09 21:09:16 -0400

    updated backend

--- swat-debates/modules/mongodb.js
+++ swat-debates/modules/mongodb.js
@@ -1,8 +1,9 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
 const db = "SwatDebates";
 const debatesCollection = "debates";
 
 export function initMongoDB() {
-    import { MongoClient, ServerApiVersion } from {'mongodb'};
 
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     const client = new MongoClient(process.env.MONGODB_URI, {
